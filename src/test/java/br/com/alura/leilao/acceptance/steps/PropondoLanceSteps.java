f9196ba7b8e2642075a39250d4410fb15f4a4b8f
Matheus Diefenthaeler <matheus.axo@gmail.com>
2021-08-04 10:53:42 -0300

    Scenario Outline, Examples

--- src/test/java/br/com/alura/leilao/acceptance/steps/PropondoLanceSteps.java
+++ src/test/java/br/com/alura/leilao/acceptance/steps/PropondoLanceSteps.java
@@ -3,6 +3,7 @@ package br.com.alura.leilao.acceptance.steps;
 import br.com.alura.leilao.model.Lance;
 import br.com.alura.leilao.model.Leilao;
 import br.com.alura.leilao.model.Usuario;
+import io.cucumber.datatable.DataTable;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.java.pt.Dado;
@@ -12,6 +13,8 @@ import org.junit.Assert;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 public class PropondoLanceSteps {
     private Lance lance;
@@ -72,6 +75,34 @@ public class PropondoLanceSteps {
         Assert.assertEquals(this.lista.size(), leilao.getLances().size());
         Assert.assertEquals(this.lista.get(0).getValor(), leilao.getLances().get(0).getValor());
         Assert.assertEquals(this.lista.get(1).getValor(), leilao.getLances().get(1).getValor());
+    }
+
+    @Dado("um lance invalido de {double} reais e do usuario {string}")
+    public void um_lance_invalido_de_reais(Double double1, String nomeUsuario) {
+        this.lance = new Lance(new BigDecimal(double1));
+    }
+
+    @Entao("o lances nao eh aceito")
+    public void o_lances_nao_eh_aceito() {
+        Assert.assertEquals(0, leilao.getLances().size());
+    }
+
+    @Dado("dois lances")
+    public void dois_lances(DataTable dataTable) {
+        List<Map<String, String>> valores = dataTable.asMaps();
+        for (Map<String, String> mapa : valores) {
+
+            String valor = mapa.get("valor");
+            String nome = mapa.get("nomeUsuario");
+            Lance lance = new Lance(new Usuario(nome), new BigDecimal(valor));
+            lista.add(lance);
+        }
+    }
+
+    @Entao("o segundo lance nao eh aceito")
+    public void o_segundo_lance_nao_eh_aceito() {
+        Assert.assertEquals(1, leilao.getLances().size());
+        Assert.assertEquals(this.lista.get(0).getValor(), leilao.getLances().get(0).getValor());
     }
 
 }
--- src/test/resources/features/propondo_lances.feature
+++ src/test/resources/features/propondo_lances.feature
@@ -12,3 +12,21 @@ Funcionalidade: Propondo lances ao leilao
     E um lance de 15.0 reais do usuario "beltrano"
     Quando propoem varios lances ao leilao
     Entao os lances sao aceitos
+
+  Esquema do Cenario: Propondo um lance invalido
+    Dado um lance invalido de <valor> reais e do usuario '<nomeUsuario>'
+    Quando propoem ao leilao
+    Entao o lances nao eh aceito
+
+    Exemplos:
+      | valor | nomeUsuario |
+      | 0.0   | beltrano    |
+      | -1.0  | cigano      |
+
+  Cenario: Propondo uma sequencia de lances
+    Dado dois lances
+      | valor | nomeUsuario |
+      | 10.0  | beltrano    |
+      | 15.0  | beltrano    |
+    Quando propoem varios lances ao leilao
+    Entao o segundo lance nao eh aceito
